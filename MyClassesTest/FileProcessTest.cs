41b651cdda21fe1a062c5042aca3cd6e5a44b949
John Flynn <40461634+Fullsailer@users.noreply.github.com>
2021-11-16 15:21:39 -0500

    Added Test Base Class
    Added Test base class  set up FileProcessTest class to inherit from test base and moved TestContect, _GoodFileName, and SetGoodFileName method to TestBase class

--- MyClassesTest/FileProcessTest.cs
+++ MyClassesTest/FileProcessTest.cs
@@ -6,22 +6,11 @@ using System.IO;
 namespace MyClassesTest
 {
     [TestClass]
-    public class FileProcessTest
+    public class FileProcessTest : TestBase
     {
-        protected string _GoodFileName;
+        
         private const string BAD_FILE_NAME = @"C:\NotExists.bad";
         
-        public TestContext TestContext { get; set; }
-
-        protected void SetGoodFileName()
-        {
-            _GoodFileName = TestContext.Properties["GoodFileName"].ToString();
-            if (_GoodFileName.Contains("[AppPath"))
-            {
-                _GoodFileName = _GoodFileName.Replace("[AppPath]",
-                    Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData));
-            }
-        }
         
         [TestMethod]
         public void FileNameDoesExists()
